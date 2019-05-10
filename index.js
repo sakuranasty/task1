f342ea5db48af6bc9c31e0f1fe7b5ecb46de62c7
Anastasiya Pavlova <34821835+sakuranasty@users.noreply.github.com>
2019-05-11 01:32:24 +0300

    airbnb style rules
    tried to apply airbnb style guide

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
-window.onload= function(){//программа должна начать работать после загрузки страницы
-    let num1=document.getElementById('num1')//сохраняем поле 1 в переменную
-    let num2=document.getElementById('num2')//сохраняем поле 2 в переменную
-    let submit=document.querySelector('input[value=Сложить]')//сохраняем в переменную кнопку
-    let resfield=document.getElementById('result');//сохраняем в переменную поле с результатом
-        function addstuff(num1,num2, field){//создаем функцию для сложения чисел
-           let a=Number(num1.value.replace(',','.').replace(' ',''))//преобразуем строку в число,заменяем запятые на точки если пользователь ввел число таким образом
-           let b=Number(num2.value.replace(',','.').replace(' ',''))
-           let flag=false//переменная-переключатель,для читабельности
-           let res//для читабельности
-           flag = (Number.isNaN(a)||Number.isNaN(b))//проверяем произошло ли преобразование в число
-           if(flag){//если преобразование не прошло
-               alert('Введите числовые значения')
-               field.innerHTML="сложение не удалось"//выводим сообщение об ошибке и в поле результата пишем что сложение не удалось
+window.onload = () =>{//программа должна начать работать после загрузки страницы
+    //сохраняем поле 1 в переменную
+    const num1 = document.getElementById('num1');
+
+    //сохраняем поле 2 в переменную
+    const num2 = document.getElementById('num2');
+
+    // сохраняем в переменную кнопку
+    let submit = document.querySelector('input[value=Сложить]');
+
+    // сохраняем в переменную поле с результатом
+    let resfield = document.getElementById('result');
+
+    // создаем функцию для сложения чисел
+        function addStuff(num1,num2, field) {
+            // преобразуем строку в число,заменяем запятые на точки если пользователь ввел число таким образом
+           let a = Number(num1.value.replace(',','.').replace(' ',''));
+
+           let b = Number(num2.value.replace(',','.').replace(' ',''));
+
+           // переменная-переключатель,для читабельности
+           let checkIfisNaN=(Number.isNaN(a)||Number.isNaN(b));
+           
+           // для сохранения результата
+           let res;
+           
+           // если преобразование не прошло
+           if(checkIfisNaN)  { 
+               alert('Введите числовые значения');
+
+               //выводим сообщение об ошибке и в поле результата пишем что сложение не удалось
+               field.innerHTML = "сложение не удалось";
+           } else { 
+              res = a + b;
+              
+              // записываем результат в поле
+              field.innerHTML = res;
            }
-           else{//есл преобразование прошло успешно
-              res=a+b
-              field.innerHTML=res//записываем результат в поле
-           }
+
            return
-        }
-    submit.addEventListener('click', function(){//подвешиваем событие на элемент
-        addstuff(num1,num2,resfield)//вызываем функцию с параметрами таким образом
-    })
-}
+        };
+
+    // подвешиваем событие на элемент
+    submit.addEventListener('click', () => {
+        // вызываем функцию с параметрами таким образом
+        addStuff(num1,num2,resfield);
+    });
+};
